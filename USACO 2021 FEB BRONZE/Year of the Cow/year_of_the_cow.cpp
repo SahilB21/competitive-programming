5f805eb566dd3cebade4bed4e3e723568029edcf
Sahil Bhatia <43319082+SahilB21@users.noreply.github.com>
2021-03-27 21:26:07 -0400

    Cleaned up 2nd attempt Year of the Cow
    Cleaned up code for the 2nd attempt of the Year of the Cow USACO problem

--- USACO 2021 FEB BRONZE/Year of the Cow/year_of_the_cow.cpp
+++ USACO 2021 FEB BRONZE/Year of the Cow/year_of_the_cow.cpp
@@ -23,77 +23,6 @@ int main(int argc, char *argv[]) {
   yearsBirth["Bessie"] = 2021;
   cowBirthMonth["Bessie"] = "Ox";
 
-  /*
-  for (int i = 0; i < n*8; i++) {
-    std::cin >> words;
-    all_words.push_back(words);
-  }
-  for (int t = 0; t < n; t++) {
-    key_words.push_back(all_words[t*8]);
-    key_words.push_back(all_words[3+(t*8)]);
-    key_words.push_back(all_words[4+(t*8)]);
-    key_words.push_back(all_words[7+(t*8)]);
-  }
-
-  for (int k = 0; k < n; k++) {
-    if (bessie_found == false) {
-      if (key_words[3+(k*4)] == "Bessie") {
-        bessie_index = k;
-        if (key_words[1+(k*4)] == "previous") {
-          for (int s = 0; s < 12; s++) {
-            if (year_cycle[s] == key_words[2+(k*4)]) {
-              years += 12-s;
-              bessie_found = true;
-            }
-          }
-        }
-        else {
-          for (int s = 0; s < 12; s++) {
-            if (year_cycle[s] == key_words[2+(k*4)]) {
-              years -= 12-s;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  while (elsie == false) {
-    for (int f = 0; f < n; f++) {
-      //std::cout << "incrementing f";
-      if (key_words[f*4] == "Elsie") {
-        elsie = true;
-      }
-      if (key_words[3+(f*4)] == key_words[bessie_index*4]) {
-        bessie_index = f;
-        if (key_words[1+(f*4)] == "previous") {
-          for (int r = 0; r < 12; r++) {
-            if (year_cycle[r] == key_words[2+(f*4)]) {
-              for (int o = 0; o < 12; o++) {
-                if (year_cycle[o] == key_words[2+((f-1)*4)]) {
-                  years += o+(12-r);
-                }
-              }
-            }
-          }
-        }
-        else {
-          for (int r = 0; r < 12; r++) {
-            if (year_cycle[r] == key_words[2+(f*4)]) {
-              for (int o = 0; o < 12; o++) {
-                if (year_cycle[o] == key_words[2+((f-1)*4)]) {
-                  years -= abs(o-(12-r));
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  std::cout << years;
-  */
 
   // Inputs each sentence into its own vector in the vector of sentences
   for (int i = 0; i < n; i++) {
@@ -106,6 +35,7 @@ int main(int argc, char *argv[]) {
     sentenceWords.clear();
   }
 
+  // Goes through each sentence
   for (int k = 0; k < n; k++) {
     cow1 = sentences[k][0];
     cow2 = sentences[k][7];
@@ -113,18 +43,6 @@ int main(int argc, char *argv[]) {
     time = sentences[k][3];
     // Finds the index of the 2nd cow (the context of the sentence is based off the position of the 2nd cow)
     for (int t = 0; t < 12; t++) {
-      //if (yearName == yearCycle[t]) {
-        //if (time == "previous") {
-          //for (int l = 0; l < 12; l++) {
-            //if (cowBirthMonth[cow2] == yearCycle[l]) {
-              //yearsBirth[cow1] = yearsBirth[cow2] + l;
-            //}
-          //}
-        //}
-        //else {
-          //yearsBirth[cow1] = yearsBirth[cow2] + t;
-        //}
-      //}
       if (cowBirthMonth[cow2] == yearCycle[t]) {
         if (time == "next") {
           // Starts going ahead for the rest of the 12 year cycle until you reach the 1st cow's birth year
@@ -135,6 +53,7 @@ int main(int argc, char *argv[]) {
           }
         }
         else {
+          // Starts going back for the rest of the 12 year cycle until you reach the 1st cow's birth year
           for (int s = 12+t; s > 0+t; s--) {
             if (cowBirthMonth[cow1] == yearCycle[s%12]) {
               yearsBirth[cow1] = yearsBirth[cow2];
@@ -145,7 +64,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-
+  // Checks which order to put the result
   if (yearsBirth["Bessie"] - yearsBirth["Elsie"] >= 0) {
     std::cout << yearsBirth["Bessie"] - yearsBirth["Elsie"];
   }

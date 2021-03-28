4cd787b65805a29fcc42ef79d22324661e6f4cff
Sahil Bhatia <43319082+SahilB21@users.noreply.github.com>
2021-03-27 21:19:48 -0400

    Completed attempt 2 for Year of the Cow
    Completed attempt 2 of Year of the Cow USACO problem

--- USACO 2021 FEB BRONZE/Year of the Cow/year_of_the_cow.cpp
+++ USACO 2021 FEB BRONZE/Year of the Cow/year_of_the_cow.cpp
@@ -3,15 +3,27 @@
 int main(int argc, char *argv[]) {
   int n;
   std::cin >> n;
-  std::string words;
-  std::vector<std::string> all_words;
-  std::vector<std::string> key_words;
-  std::string year_cycle[12] = {"Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig", "Rat"};
-  bool elsie = false;
-  int years = 0;
-  int bessie_index;
-  bool bessie_found = false;
+  //std::string words;
+  //std::vector<std::string> all_words;
+  //std::vector<std::string> key_words;
+  std::string yearCycle[12] = {"Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig", "Rat"};
+  //bool elsie = false;
+  //int years = 0;
+  //int bessie_index;
+  //bool bessie_found = false;
+  std::map<std::string, int> yearsBirth;
+  std::map<std::string, std::string> cowBirthMonth;
+  std::vector<std::string> sentenceWords;
+  std::vector< std::vector<std::string> > sentences;
+  std::string cow1;
+  std::string cow2;
+  int year_num = 0;
+  std::string time;
 
+  yearsBirth["Bessie"] = 2021;
+  cowBirthMonth["Bessie"] = "Ox";
+
+  /*
   for (int i = 0; i < n*8; i++) {
     std::cin >> words;
     all_words.push_back(words);
@@ -81,6 +93,65 @@ int main(int argc, char *argv[]) {
   }
 
   std::cout << years;
+  */
+
+  // Inputs each sentence into its own vector in the vector of sentences
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < 8; j++) {
+      std::string word;
+      std::cin >> word;
+      sentenceWords.push_back(word);
+    }
+    sentences.push_back(sentenceWords);
+    sentenceWords.clear();
+  }
+
+  for (int k = 0; k < n; k++) {
+    cow1 = sentences[k][0];
+    cow2 = sentences[k][7];
+    cowBirthMonth[cow1] = sentences[k][4];
+    time = sentences[k][3];
+    // Finds the index of the 2nd cow (the context of the sentence is based off the position of the 2nd cow)
+    for (int t = 0; t < 12; t++) {
+      //if (yearName == yearCycle[t]) {
+        //if (time == "previous") {
+          //for (int l = 0; l < 12; l++) {
+            //if (cowBirthMonth[cow2] == yearCycle[l]) {
+              //yearsBirth[cow1] = yearsBirth[cow2] + l;
+            //}
+          //}
+        //}
+        //else {
+          //yearsBirth[cow1] = yearsBirth[cow2] + t;
+        //}
+      //}
+      if (cowBirthMonth[cow2] == yearCycle[t]) {
+        if (time == "next") {
+          // Starts going ahead for the rest of the 12 year cycle until you reach the 1st cow's birth year
+          for (int l = t; l < 12+t; l++) {
+            if (cowBirthMonth[cow1] == yearCycle[l%12]) {
+              yearsBirth[cow1] = yearsBirth[cow2] + l;
+            }
+          }
+        }
+        else {
+          for (int s = 12+t; s > 0+t; s--) {
+            if (cowBirthMonth[cow1] == yearCycle[s%12]) {
+              yearsBirth[cow1] = yearsBirth[cow2];
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  if (yearsBirth["Bessie"] - yearsBirth["Elsie"] >= 0) {
+    std::cout << yearsBirth["Bessie"] - yearsBirth["Elsie"];
+  }
+  else {
+    std::cout << yearsBirth["Elsie"] - yearsBirth["Bessie"];
+  }
 
   return 0;
 }

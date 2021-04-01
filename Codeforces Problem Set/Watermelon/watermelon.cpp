8a8fff12eb05051c6ffaca5af346b53e7e7b6e82
Sahil Bhatia <43319082+SahilB21@users.noreply.github.com>
2021-03-31 23:06:18 -0500

    Completed Watermelon problem
    Started and completed Codeforce's problem set 4A Watermelon problem

---
+++ Codeforces Problem Set/Watermelon/watermelon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char *argv[]) {
+
+  int w;
+  std::cin >> w;
+
+
+  if (w > 2 && w % 2 == 0) {
+    std::cout << "YES" << std::endl;
+    return 0;
+  }
+  std::cout << "NO" << std::endl;
+
+
+  return 0;
+}
